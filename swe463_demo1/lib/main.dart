28c6a3bfdeb98233ea452f24bf322e79f695a57e
Hady AL Saleem <hadisaleem50@gmail.com>
2026-08-26 21:27:01 +0300

    done with checkoint 3

--- swe463_demo1/lib/main.dart
+++ swe463_demo1/lib/main.dart
@@ -8,15 +8,59 @@ import 'package:flutter/material.dart';
 //  );
 // }
 
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Welcome to SWE 463', style: TextStyle(fontSize: 24)),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Welcome to SWE 463', style: TextStyle(fontSize: 24)),
+        appBar: AppBar(
+          title: const Text('SWE 463 - Demo 1'),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white
         ),
+        drawer: const Drawer(
+        child: SafeArea(
+        child: Column(
+        children: [
+          DrawerHeader(
+
+            child: Center(
+              child: Text(
+                'Demo Menu',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+        ],
+                ),
+            ),
+          ), 
+          body: const Center(
+            child: Text(
+            'Open the drawer from the menu icon',
+            style: TextStyle(fontSize: 20),
+                  ),
+ 
+          ),
       ),
     ),
   );
 }
+
