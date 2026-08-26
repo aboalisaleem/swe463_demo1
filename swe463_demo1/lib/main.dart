a1a6ee6c45b5a99b5c3d268151d7090f43b9c5e5
Hady AL Saleem <hadisaleem50@gmail.com>
2026-08-26 20:37:44 +0300

    Checkpoint 2 done

--- swe463_demo1/lib/main.dart
+++ swe463_demo1/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('My name is HADY AL SALEEM')));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Welcome to SWE 463', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    ),
+  );
 }
