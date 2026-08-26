3e39d58ad1ad4d6c6b029e00e660b9c968e04886
Hady AL Saleem <hadisaleem50@gmail.com>
2026-08-26 21:28:52 +0300

    comment checlpoint3

--- swe463_demo1/lib/main.dart
+++ swe463_demo1/lib/main.dart
@@ -21,46 +21,50 @@ import 'package:flutter/material.dart';
 //   );
 // }
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SWE 463 - Demo 1'),
-          backgroundColor: Colors.indigo,
-          foregroundColor: Colors.white
-        ),
-        drawer: const Drawer(
-        child: SafeArea(
-        child: Column(
-        children: [
-          DrawerHeader(
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('SWE 463 - Demo 1'),
+//           backgroundColor: Colors.indigo,
+//           foregroundColor: Colors.white
+//         ),
+//         drawer: const Drawer(
+//         child: SafeArea(
+//         child: Column(
+//         children: [
+//           DrawerHeader(
 
-            child: Center(
-              child: Text(
-                'Demo Menu',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-        ],
-                ),
-            ),
-          ), 
-          body: const Center(
-            child: Text(
-            'Open the drawer from the menu icon',
-            style: TextStyle(fontSize: 20),
-                  ),
+//             child: Center(
+//               child: Text(
+//                 'Demo Menu',
+//                 style: TextStyle(fontSize: 24),
+//               ),
+//             ),
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.home),
+//             title: Text('Home'),
+//           ),
+//         ],
+//                 ),
+//             ),
+//           ), 
+//           body: const Center(
+//             child: Text(
+//             'Open the drawer from the menu icon',
+//             style: TextStyle(fontSize: 20),
+//                   ),
  
-          ),
-      ),
-    ),
-  );
-}
+//           ),
+//       ),
+//     ),
+//   );
+// }
+
+
+
+
 
