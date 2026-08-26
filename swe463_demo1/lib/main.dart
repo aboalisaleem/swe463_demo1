914639ff71306366b865537d9195947834d184cb
Hady AL Saleem <hadisaleem50@gmail.com>
2026-08-26 22:36:51 +0300

    upload fifth screenshot

--- swe463_demo1/lib/main.dart
+++ swe463_demo1/lib/main.dart
@@ -64,37 +64,40 @@ import 'package:flutter/material.dart';
 //   );
 // }
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SWE 463 - Demo 1'),
-        ),
-        drawer: const Drawer(
-        child: SafeArea(
-          child: ListTile(
-            leading: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-          ),
-        ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('SWE 463 - Demo 1'),
+//         ),
+//         drawer: const Drawer(
+//         child: SafeArea(
+//           child: ListTile(
+//             leading: Icon(Icons.home),
+//               title: Text('Home'),
+//             ),
+//           ),
+//         ),
  
-        body: const Center(
-          child: Text(
-            'MyApp is running',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//         body: const Center(
+//           child: Text(
+//             'MyApp is running',
+//             style: TextStyle(fontSize: 24),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
